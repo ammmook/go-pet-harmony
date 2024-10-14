a84ac1e0b49f26386f4220c7c108637443720c20
f1nn-ach <fsl.finscarlett@gmail.com>
2024-10-15 03:01:31 +0700

    So sleeppy

--- controller/userController.go
+++ controller/userController.go
@@ -1,19 +1,113 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"text/template"
 
 	managementdb "github.com/f1nn-ach/pj-golang/managementDB"
+	"github.com/f1nn-ach/pj-golang/model"
 )
 
-func CallUser() {
-	user, err := managementdb.GetUsersByEmail("user")
+type Messege map[string]string
 
-	if err != nil {
-		log.Fatalf("Error retrieving user: %v", err)
+func UserRegister(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		user := model.User{
+			Email:       r.FormValue("email"),
+			Firstname:   r.FormValue("firstname"),
+			Lastname:    r.FormValue("lastname"),
+			PhoneNumber: r.FormValue("phoneNumber"),
+			Password:    r.FormValue("password"),
+		}
+
+		managementdb.AddtUsers(user)
+
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+	} else if r.Method == http.MethodGet {
+		http.ServeFile(w, r, "view/register.html")
 	}
+}
 
-	fmt.Printf("Uses: %s\n", user.Email)
-	fmt.Printf("Role: %s\n", user.Role)
+func UserLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		email := r.FormValue("email")
+		pwd := r.FormValue("password")
+
+		if email == "" || pwd == "" {
+			tmpl, err := template.ParseFiles("view/login.html")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			mess := Messege{
+				"message": "Email and password cannot be empty",
+			}
+			tmpl.Execute(w, mess)
+		}
+
+		user, err := managementdb.GetUserByEmail(email)
+		if err != nil || user == nil {
+			tmpl, err := template.ParseFiles("view/login.html")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			mess := Messege{
+				"message": "User not found",
+			}
+			tmpl.Execute(w, mess)
+		}
+
+		if user.Password != pwd {
+			tmpl, err := template.ParseFiles("view/login.html")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			mess := Messege{
+				"message": "Invalid email or password",
+			}
+			tmpl.Execute(w, mess)
+		} else {
+			tmpl, err := template.ParseFiles("view/result.html")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			err = tmpl.Execute(w, user)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	} else {
+		http.ServeFile(w, r, "view/login.html")
+	}
+}
+
+func GetResultPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.ServeFile(w, r, "view/result.html")
+	}
+}
+
+func CallUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		usersJSON, err := managementdb.GetAllUsers()
+		if err != nil {
+			log.Fatalf("Error getting users: %v", err)
+		}
+
+		var users []model.User
+		if err := json.Unmarshal(usersJSON, &users); err != nil {
+			log.Fatalf("Error unmarshalling users: %v", err)
+		}
+
+		for _, user := range users {
+			fmt.Println(user.Firstname)
+			w.Write(usersJSON)
+		}
+	}
 }
--- managementDB/userManagement.go
+++ managementDB/userManagement.go
@@ -1,39 +1,89 @@
 package managementdb
 
 import (
+	"database/sql"
 	"encoding/json"
-	"fmt"
-	"net/http"
 
 	"github.com/f1nn-ach/pj-golang/initializers"
 	"github.com/f1nn-ach/pj-golang/model"
 )
 
-func GetUsers(w http.ResponseWriter, r *http.Request) {
+func GetAllUsers() ([]byte, error) {
+	db := initializers.OpenConnection()
+	defer db.Close()
+
 	var users []model.User
-	if r.Method == http.MethodGet {
-		db := initializers.OpenConnection()
-		result, _ := db.Query("select * from users")
-		for result.Next() {
-			var user model.User
-			result.Scan(&user.Email, &user.Firstname, &user.Lastname, &user.Password, &user.PhoneNumber, &user.Role)
-			users = append(users, user)
-		}
-		res, _ := json.Marshal(users)
-		fmt.Fprint(w, (string(res)))
-
-		defer db.Close()
+	result, err := db.Query("SELECT * FROM Users")
+	if err != nil {
+		return nil, err
 	}
+	defer result.Close()
+
+	for result.Next() {
+		var user model.User
+		var role sql.NullString
+		if err := result.Scan(&user.Email, &user.Firstname, &user.Lastname, &user.Password, &user.PhoneNumber, &role); err != nil {
+			return nil, err
+		}
+
+		if role.Valid {
+			user.Role = role.String
+		} else {
+			user.Role = "default"
+		}
+
+		users = append(users, user)
+	}
+
+	res, err := json.Marshal(users)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
-func PostUsers(w http.ResponseWriter, r *http.Request) {
+func AddtUsers(user model.User) (sql.Result, error) {
+	db := initializers.OpenConnection()
+	defer db.Close()
 
+	query := "INSERT INTO Users (email, firstname, lastname, phone_number, password) VALUES (?, ?, ?, ?, ?)"
+	result, err := db.Exec(query,
+		user.Email,
+		user.Firstname,
+		user.Lastname,
+		user.PhoneNumber,
+		user.Password,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
 
-func PutUsers(w http.ResponseWriter, r *http.Request) {
+func GetUserByEmail(email string) (*model.User, error) {
+	db := initializers.OpenConnection()
+	defer db.Close()
 
-}
+	query := "SELECT * FROM Users WHERE email = ?"
 
-func DeleteUsers(w http.ResponseWriter, r *http.Request) {
+	result := db.QueryRow(query, email)
 
+	var user model.User
+	var role sql.NullString
+
+	err := result.Scan(&user.Email, &user.Firstname, &user.Lastname, &user.PhoneNumber, &user.Password, &role)
+	if err != nil {
+		return nil, err
+	}
+
+	if role.Valid {
+		user.Role = role.String
+	} else {
+		user.Role = "default"
+	}
+
+	return &user, nil
 }
