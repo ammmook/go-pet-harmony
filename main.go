aab58f766883ddd4a36f1c06267e46b71e28da43
ammmook <ammmook04@gmail.com>
2024-10-14 19:15:35 +0700

    update html

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/f1nn-ach/pj-golang/controller"
 	"github.com/f1nn-ach/pj-golang/initializers"
 )
@@ -10,5 +12,6 @@ func init() {
 }
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("net"))))
 	controller.CallUser()
 }
