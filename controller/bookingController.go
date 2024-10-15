b24cbe8c3187589037fce35741624aefd39774e8
PhithakDrxw <zxcboom2004a@gmail.com>
2024-10-16 03:14:57 +0700

    update css

--- controller/bookingController.go
+++ controller/bookingController.go
@@ -1,39 +1,39 @@
 package controller
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	managementdb "github.com/f1nn-ach/pj-golang/managementDB"
-	"github.com/f1nn-ach/pj-golang/model"
-)
+// 	managementdb "github.com/f1nn-ach/pj-golang/managementDB"
+// 	"github.com/f1nn-ach/pj-golang/model"
+// )
 
-func BookingRegister(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		session, _ := store.Get(r, "session-name")
-		userEmail := session.Values["user"].(string)
+// func BookingRegister(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodPost {
+// 		session, _ := store.Get(r, "session-name")
+// 		userEmail := session.Values["user"].(string)
 
-		pets, err := managementdb.GetPetsByEmail(userEmail)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+// 		pets, err := managementdb.GetPetsByEmail(userEmail)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 		}
 
-		booking := model.Booking{
-			StartDate: r.FormValue("startDate"),
-		}
+// 		booking := model.Booking{
+// 			StartDate: r.FormValue("startDate"),
+// 		}
 
-	} else if r.Method == http.MethodGet {
-		session, _ := store.Get(r, "session-name")
-		userEmail := session.Values["user"].(string)
+// 	} else if r.Method == http.MethodGet {
+// 		session, _ := store.Get(r, "session-name")
+// 		userEmail := session.Values["user"].(string)
 
-		pets, err := managementdb.GetPetsByEmail(userEmail)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		data := &TemplateData{
-			Pets: pets,
-		}
-		renderTemplate(w, "booking.html", data)
-	}
-}
+// 		pets, err := managementdb.GetPetsByEmail(userEmail)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 		}
+// 		data := &TemplateData{
+// 			Pets: pets,
+// 		}
+// 		renderTemplate(w, "booking.html", data)
+// 	}
+// }
